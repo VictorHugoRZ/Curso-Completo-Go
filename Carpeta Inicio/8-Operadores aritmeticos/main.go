ac79a82478d69f28aa6e2e4241eba85f5086d0e0
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-08-11 17:57:53 -0600

    Clase 8 Operadores aritmeticos

---
+++ Carpeta Inicio/8-Operadores aritmeticos/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(59 + 41) //100
+
+	fmt.Println(99 - 9) //90
+
+	fmt.Println(8 * 4) //32
+
+	fmt.Println(50 / 5) //10
+
+	fmt.Println(10 % 2) //0
+}
