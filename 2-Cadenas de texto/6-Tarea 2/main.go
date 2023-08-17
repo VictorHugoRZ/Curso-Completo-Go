897a65d353eff1de442d7014c588ac7ac5e7b99b
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-08-17 16:15:54 -0600

    Tarea 2

---
+++ 2-Cadenas de texto/6-Tarea 2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre string
+	var direccion string
+	var edad int8
+	var profesion string
+
+	fmt.Println("Introduce tu nombre: " + nombre)
+	fmt.Scanln(&nombre)
+
+	fmt.Println("Introduce tu direccion: " + direccion)
+	fmt.Scanln(&direccion)
+
+	fmt.Println("Introduce tu edad: ", edad)
+	fmt.Scanln(&edad)
+
+	fmt.Println("Introduce tu profesion: " + profesion)
+	fmt.Scanln(&profesion)
+}
