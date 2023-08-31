bfda419ead41b5626945f2c88ae2a2bb30a237b4
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-08-30 19:50:48 -0600

    Operadores Relacionales

--- 3-Booleanos/2-Operadores Relacionales/main.go
+++ 3-Booleanos/2-Operadores Relacionales/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola")
+	num1 := 20
+	num2 := 30
+	fmt.Println(num1 < num2) //true
+	fmt.Println(num1 > num2) //false
+
+	num1 = 30
+	fmt.Println(num1 <= num2) //true
+
+	num1 = 31
+	fmt.Println(num1 >= num2) //true
+	fmt.Println(num1 == num2) //false
+	fmt.Println(num1 != num2) //true
 }
