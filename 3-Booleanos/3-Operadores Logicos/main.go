10933f65bfc7e44e278e41aa46f9079781991b99
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-08-30 20:00:48 -0600

    Operadores Logicos

---
+++ 3-Booleanos/3-Operadores Logicos/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 10
+	num2 := 20
+	num3 := 30
+	num4 := 40
+
+	fmt.Println(num1 < num2 && num3 == num4) //false
+	fmt.Println(num1 != num2 || num3 > num4) //true
+}
