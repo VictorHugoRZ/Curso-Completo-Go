58f1414ee7486657473752e8965e53b62ae78c5e
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-09-05 20:37:29 -0600

    Keyword Defer

---
+++ 8-Keywords/3-Defer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Defer
+	//La palabra clave defer nos permite "saltar" una seccion de codigo determinada para que esta se ejecute
+	//despues de todo el codigo siguiente
+
+	defer fmt.Println("Este mensaje va primero")
+	fmt.Println("Este mensaje va segundo")
+	fmt.Println("Este mensaje va tercero")
+	fmt.Println("Este mensaje va cuarto")
+
+	// Este mensaje va segundo
+	// Este mensaje va tercero
+	// Este mensaje va primero
+	// Este mensaje va cuarto
+
+	//No se recomienda utilizar mas de un defer en nuestro codigo, ya que el efecto de este se puede volver
+	//un poco impredecible
+}
