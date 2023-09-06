2d9d1b20faa8c913d1b3add252cf7aa3a3106849
Victor Hugo Ramirez Zamora <v.zamoravictor@gmail.com>
2023-09-05 19:17:22 -0600

    Keyword Continue

--- 8-Keywords/1-Continue/main.go
+++ 8-Keywords/1-Continue/main.go
@@ -1,6 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Continue
 	//La palabra clave continue nos ayuda a omitir cierta parte de nuestro codigo
+
+	for carga := 0; carga <= 10; carga++ {
+		if carga == 4 || carga == 5 || carga == 6 {
+			continue
+		}
+		fmt.Println(carga)
+	}
+	//Se omiten los numero 4, 5, 6 al utilizar el continue
+	//1
+	//2
+	//3
+	//7
+	//8
+	//9
+	//10
 }
